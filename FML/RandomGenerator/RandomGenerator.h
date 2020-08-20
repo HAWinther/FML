29ba9e352cba6864d06a0ab75034cd2fa10204fa
Hans A. Winther <hansw@Hanss-MacBook-Pro.local>
2020-08-21 00:05:30 +0200

    Randomgenerator to header only

--- FML/RandomGenerator/RandomGenerator.h
+++ FML/RandomGenerator/RandomGenerator.h
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <cstdio>
 #include <cassert>
+#include <memory>
 #ifdef USE_GSL
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
@@ -17,107 +18,110 @@ namespace FML {
 
     // Select the type of random generator and specify the seed size
     typedef std::mt19937 std_random_generator_type;
-    const size_t std_seed_size = 624;
-    const std::string standardname = "mt19937";
+#define STDRANDOM_NAME "mt19937"
 
-    // Select type of GSL random generator
-#ifdef USE_GSL
+    // Gsl defines
+#define GSLRANDOM_NAME "gsl_rng_ranlxd1"
 #ifndef gsl_random_generator_type
 #define gsl_random_generator_type gsl_rng_ranlxd1
 #endif
-#endif
-
-    // Default ranges for the uniform and normal distribution
-    const double fiducial_uniform_range_lower = 0.0;
-    const double fiducial_uniform_range_upper = 1.0;
-    const double fiducial_normal_mu           = 0.0;
-    const double fiducial_normal_sigma        = 1.0;
 
     class RandomGenerator {
       protected:
 
         std::vector<unsigned int> Seed;
         std_random_generator_type generator;
-        std::uniform_real_distribution<double> uniform_dist;
-        std::normal_distribution<double> normal_dist;
-
-        std::string name;
-
-        double uniform_range_lower;
-        double uniform_range_upper;
-        double normal_mu;
-        double normal_sigma;
+        std::uniform_real_distribution<double> uniform_dist{0.0,1.0};
+        std::normal_distribution<double> normal_dist{0.0,1.0};
+        std::string name{STDRANDOM_NAME};
 
       public:
 
-        RandomGenerator();
-        RandomGenerator(unsigned int Seed);
-        RandomGenerator(std::vector<unsigned int> Seed);
-        ~RandomGenerator();
-        RandomGenerator(const RandomGenerator &rhs);
-
-        RandomGenerator& operator=(const RandomGenerator &rhs){
-          Seed = rhs.Seed;
-          generator = rhs.generator;
-          uniform_range_lower = rhs.uniform_range_lower;
-          uniform_range_upper = rhs.uniform_range_upper;
-          uniform_dist = rhs.uniform_dist;
-          normal_mu = rhs.normal_mu;
-          normal_sigma = rhs.normal_sigma;
-          normal_dist = rhs.normal_dist;
-          return *this;
+        RandomGenerator() = default;
+        virtual ~RandomGenerator() = default;
+        
+        RandomGenerator(unsigned int seed){
+          Seed = std::vector<unsigned int>(624,seed);;
+          std::seed_seq seeds(begin(Seed), end(Seed));
+          generator = std_random_generator_type(seeds);
         }
 
-        virtual void free(){}
-        virtual void set_seed(std::vector<unsigned int> Seed);
-        virtual void set_seed(unsigned int Seed);
-        virtual void set_uniform_range(double a, double b);
-        virtual void set_normal_range(double mu, double sigma);
-        virtual double generate_uniform();
-        virtual double generate_normal();
+        RandomGenerator(std::vector<unsigned int> seed){
+          set_seed(seed);
+        }
+
+        virtual void set_seed(std::vector<unsigned int> seed){
+          Seed = seed;
+          std::seed_seq seeds(begin(Seed), end(Seed));
+          generator = std_random_generator_type(seeds);
+        }
+
+        virtual void set_seed(unsigned int Seed){
+          set_seed(std::vector<unsigned int>(624,Seed));
+        } 
+
+        virtual double generate_uniform(){ 
+          return uniform_dist(generator); 
+        }
+
+        virtual double generate_normal(){
+          return normal_dist(generator);
+        }
+
     };
+    
+    //=======================================================================
+    //=======================================================================
 
 #ifdef USE_GSL
+
+    struct GSLRNG{
+      gsl_rng * random_generator{nullptr};
+      GSLRNG(){
+        random_generator = gsl_rng_alloc(gsl_random_generator_type);
+      }
+      ~GSLRNG(){
+        if(random_generator != nullptr)
+          gsl_rng_free(random_generator);
+      }
+    };
+
     class GSLRandomGenerator : public RandomGenerator {
       private:
 
-        gsl_rng *random_generator;
-        bool allocated; 
+        GSLRNG rng;
 
       public:
 
-        GSLRandomGenerator(int seed);
-        GSLRandomGenerator();
-        ~GSLRandomGenerator();
-        GSLRandomGenerator(const GSLRandomGenerator &rhs);
-
-        GSLRandomGenerator& operator=(const GSLRandomGenerator &rhs){
-          if(this != &rhs){
-            if(rhs.allocated){ 
-              if(allocated) free();
-              random_generator = gsl_rng_alloc(gsl_rng_ranlxd1); 
-              allocated = true;
-              memcpy(random_generator->state, rhs.random_generator->state, rhs.random_generator->type->size);
-            } else {
-              free();
-            }
-            uniform_range_lower = rhs.uniform_range_lower;
-            uniform_range_upper = rhs.uniform_range_upper;
-            normal_mu = rhs.normal_mu;
-            normal_sigma = rhs.normal_sigma;
-          }
-          return *this;
+        GSLRandomGenerator(){
+          name = GSLRANDOM_NAME;
         }
 
-        void free();
-        void set_seed(unsigned int seed);
-        void set_uniform_range(double a, double b);
-        void set_normal_range(double mu, double sigma);
-        double generate_uniform();
-        double generate_normal();
+        GSLRandomGenerator(int seed) {
+          name = GSLRANDOM_NAME;
+          set_seed(seed);
+        }
+
+        void set_seed(unsigned int seed){
+          Seed = std::vector<unsigned int>(1,seed);
+          gsl_rng_set(rng.random_generator, seed);
+        }
+
+        double generate_uniform(){
+          return gsl_rng_uniform(rng.random_generator);
+        }
+
+        double generate_normal(){
+          return gsl_ran_gaussian(rng.random_generator,1.0);
+        }
+    
     };
+
 #endif
+
+    //=======================================================================
+    //=======================================================================
+
   }
 }
-
 #endif
--- FML/RandomGenerator/example/Main.cpp
+++ FML/RandomGenerator/example/Main.cpp
@@ -11,13 +11,11 @@ int main(){
   //=========================================
 
   FML::RANDOM::RandomGenerator r;
-  r.set_uniform_range(2.0, 5.0);
   std::cout << r.generate_uniform() << "\n";
 
 #ifdef USE_GSL
   // If you want to use GSL
   std::shared_ptr<FML::RANDOM::RandomGenerator> r1 = std::make_shared<FML::RANDOM::GSLRandomGenerator> ();
-  r1->set_uniform_range(2.0, 5.0);
   std::cout << r1->generate_uniform() << "\n";
 #endif
 }
