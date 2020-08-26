8f91cd8100997129caa2f54b7fe90eba2bfcb91b
Hans A. Winther <hansw@eduroam-193-157-170-143.wlan.uio.no>
2020-08-26 16:21:17 +0200

    Updated LPT example

--- FML/LPT/example/Main.cpp
+++ FML/LPT/example/Main.cpp
@@ -8,24 +8,44 @@
 
 //=====================================================
 //
-// In this example we generate a gaussian random field
-// delta from a given power-spectrum
+// In this example we do the following
 //
-// From this we compute the LPT displacement field to
+// 1. Generate a gaussian random field
+// delta from a given power-spectrum.
+//
+// 2. Compute the LPT displacement field to
 // second order
 //
-// We use this to generate particle positions
-// The 2LPT thing is cosmology specific
+// 3. Generate particle positions
+// and velocity (we use v_code = dx/dt / (H0 Box) )
+// (The velocity and 2LPT is cosmology specific)
 //
-// Then we compute the power-spectrum of this particle
+// 4. Compute the power-spectrum of this particle
 // distribution and compare it to the input P(k)
 //
-// So this test is a simple IC generator for N-body
-// simulations
+// So this test is basically a very simple IC generator 
+// for N-body simulations.
+//
+// Relation between code units and physical units:
+// t_code = t/T = tH0 where T = 1/H0
+// x_code = x/Box
+// v_code = dx/dt / (H0 Box)
+// Thus positions are in [0,1) and the velocities are
+// comoving velocities in units of (100 * box) km/s
 //
 //=====================================================
 
+//=====================================================
+// The dimension we are working in (e.g. Ndim=2 is nice
+// to use for testing as its much faster) and the 
+// boxsize in your physical units (say Mpc/h)
+//=====================================================
 const int Ndim = 3;
+const double box = 100.0;
+
+//=====================================================
+// Type aliases for easier use below
+//=====================================================
 template <class T>
 using MPIParticles = FML::PARTICLE::MPIParticles<T>;
 template <int N>
@@ -33,18 +53,14 @@ using FFTWGrid = FML::GRID::FFTWGrid<N>;
 using Particle = SimpleParticle<Ndim>;
 
 //=====================================================
-// The boxsize in your physical units (say Mpc/h)
-//=====================================================
-const double box = 100.0;
-
-//=====================================================
-// Power-spectrum in physical units
+// Power-spectrum at the time we want to generate the
+// IC in physical units. Just a random example
 // (i.e. Units of L^NDIM where L is the units of box)
 //=====================================================
 double power_spectrum(double k) {
     if (k == 0.0)
         return 0.0;
-    return 0.00001;
+    return 1e-4;
 }
 
 //=====================================================
@@ -54,10 +70,13 @@ double power_spectrum(double k) {
 void generate_delta(FFTWGrid<Ndim> & delta) {
     FML::RANDOM::RandomGenerator * rng = new FML::RANDOM::RandomGenerator;
     std::function<double(double)> Powspec;
+    
+    // Fix amplitude (so only random phases) or do a normal GRF if false
     const bool fix_amplitude = true;
 
-    // Function P(kBox / Box) / Volume
-    Powspec = [&](double kBox) -> double { return power_spectrum(kBox / box) / std::pow(box, Ndim); };
+    // Function P(kBox / Box) / Volume that the method below requires
+    const double volume = std::pow(box, Ndim);
+    Powspec = [&](double kBox) -> double { return power_spectrum(kBox / box) / volume; };
 
     // Make a random field in fourier space
     FML::RANDOM::GAUSSIAN::generate_gaussian_random_field_fourier(delta, rng, Powspec, fix_amplitude);
@@ -68,6 +87,10 @@ int main() {
     auto * mem = FML::MemoryLog::get();
 #endif
 
+    //=====================================================
+    // Setting: what grid to use to generate the IC on and 
+    // how many particles to generate
+    //=====================================================
     const int Nmesh = 128;
     const int Npart_1D = 128;
     const double buffer_factor = 1.25;
@@ -82,13 +105,13 @@ int main() {
     generate_delta(delta);
 
     //=====================================================
-    // Generate phi_1LPT = delta(k)/k^2
+    // Generate the 1LPT potential phi_1LPT = delta(k)/k^2
     //=====================================================
     FFTWGrid<Ndim> phi_1LPT;
     FML::COSMOLOGY::LPT::compute_1LPT_potential_fourier(delta, phi_1LPT);
 
     //=====================================================
-    // Generate phi_2LPT
+    // Generate the 2LPT potential phi_2LPT
     // 7 FFTS
     //=====================================================
     FFTWGrid<Ndim> phi_2LPT;
@@ -112,15 +135,15 @@ int main() {
     phi_2LPT.free();
 
     //===============================Y======================
-    // Make a regular particle grid (with Npart_1D^NDIM
-    // particles in total)
+    // Make a regular (Lagrangian) particle grid with 
+    // Npart_1D^NDIM particles in total
     //=====================================================
     MPIParticles<Particle> part;
     part.create_particle_grid(Npart_1D, buffer_factor, FML::xmin_domain, FML::xmax_domain);
     part.info();
 
     //=====================================================
-    // Interpolate displacement field to particle positions
+    // Interpolate 1LPT displacement field to particle positions
     // (Note: if Npart_1D = Nmesh then we can assign directly
     // without interpolation using the disp fields as the
     // cell is at the particle position and save time though
@@ -136,7 +159,7 @@ int main() {
     }
 
     //=====================================================
-    // Interpolate displacement field to particle positions
+    // Interpolate 2LPT displacement field to particle positions
     //=====================================================
     std::vector<std::vector<FML::GRID::FloatType>> displacements_2LPT(Ndim);
     for (int idim = Ndim - 1; idim >= 0; idim--) {
@@ -146,6 +169,18 @@ int main() {
     }
 
     //=====================================================
+    // Set the velocities to be the dimensionless v_code = v  / (H0 Box)
+    // For simplicity for the growth rate we just use the approx D1 = a => f1 = 1 and f2 = 2
+    // (easy to compute the exact values by just solving the ODE or using the approx f1 ~ OmegaM(a)^0.55)
+    //=====================================================
+    const double a_ini = 1.0 / (1.0 + 50.0);
+    const double HoverH0 = std::sqrt(1.0 / (a_ini * a_ini * a_ini));
+    const double growth_rate1 = 1.0;
+    const double growth_rate2 = 2.0;
+    const double vfac_1LPT = HoverH0 * growth_rate1;
+    const double vfac_2LPT = HoverH0 * growth_rate2;
+    
+    //=====================================================
     // Add 1LPT displacement to particle position
     // Add 2LPT displacement to particle position
     // Note the -3/7 factor. This is because growth factors
@@ -153,19 +188,32 @@ int main() {
     // the physically relevant solution has D2 = -3/7 D1^2
     // so we need to put this in by hand
     //=====================================================
+    
     double max_disp_1LPT = 0.0;
     double max_disp_2LPT = 0.0;
+    double max_vel_1LPT = 0.0;
+    double max_vel_2LPT = 0.0;
+
     auto * part_ptr = part.get_particles_ptr();
+#ifdef USE_OMP
+#pragma omp parallel for
+#endif
     for (size_t ind = 0; ind < part.get_npart(); ind++) {
         auto * pos = part_ptr[ind].get_pos();
+        auto * vel = part_ptr[ind].get_vel();
         for (int idim = Ndim - 1; idim >= 0; idim--) {
-            double dpos_1LPT = displacements_1LPT[idim][ind];
-            double dpos_2LPT = -3.0 / 7.0 * displacements_2LPT[idim][ind];
+            const double dpos_1LPT = displacements_1LPT[idim][ind];
+            const double dpos_2LPT = -3.0 / 7.0 * displacements_2LPT[idim][ind];
             pos[idim] += dpos_1LPT + dpos_2LPT;
+            vel[idim] = vfac_1LPT * dpos_1LPT + vfac_2LPT * dpos_2LPT;
             if (std::fabs(dpos_1LPT) > max_disp_1LPT)
                 max_disp_1LPT = std::fabs(dpos_1LPT);
             if (std::fabs(dpos_2LPT) > max_disp_2LPT)
                 max_disp_2LPT = std::fabs(dpos_2LPT);
+            if (std::fabs(vfac_1LPT * dpos_1LPT) > max_vel_1LPT)
+                max_vel_1LPT = std::fabs(vfac_1LPT * dpos_1LPT);
+            if (std::fabs(vfac_2LPT * dpos_2LPT) > max_vel_2LPT)
+                max_vel_2LPT = std::fabs(vfac_2LPT * dpos_2LPT);
             if (pos[idim] >= 1.0)
                 pos[idim] -= 1.0;
             if (pos[idim] < 0.0)
@@ -176,50 +224,65 @@ int main() {
     //=====================================================
     // We no longer need Psi
     //=====================================================
-    for (int idim = Ndim - 1; idim >= 0; idim--) {
+    for (int idim = 0; idim < Ndim; idim++) {
         displacements_1LPT[idim].clear();
         displacements_1LPT[idim].shrink_to_fit();
         displacements_2LPT[idim].clear();
         displacements_2LPT[idim].shrink_to_fit();
     }
 
+    //=====================================================
+    // Output the maximum displacment and velocity
+    //=====================================================
     FML::MaxOverTasks(&max_disp_1LPT);
     FML::MaxOverTasks(&max_disp_2LPT);
+    FML::MaxOverTasks(&max_vel_1LPT);
+    FML::MaxOverTasks(&max_vel_2LPT);
     if (FML::ThisTask == 0)
         std::cout << "Maximum displacements: " << max_disp_1LPT * Nmesh << " grid cells\n";
     if (FML::ThisTask == 0)
         std::cout << "Maximum displacements: " << max_disp_2LPT * Nmesh << " grid cells\n";
+    if (FML::ThisTask == 0)
+        std::cout << "Maximum velocity: " << max_vel_1LPT * 100.0 * box << " km/s comoving\n";
+    if (FML::ThisTask == 0)
+        std::cout << "Maximum velocity: " << max_vel_2LPT * 100.0 * box << " km/s comoving\n";
 
     //=====================================================
-    // We no longer need displacements
+    // Communicate particles (they might have left the 
+    // current task)
     //=====================================================
     part.communicate_particles();
 
     //=====================================================
     // Lets test that it works as expected
-    // Compute power-spectrum with a larger grid
+    // Compute power-spectrum using a 5th order kernel to
+    // ensure we get subpercent accuracy in pofk up until the
+    // nyquist frequency (we can also just a larger grid)
     //=====================================================
+    interpolation_method = "PQS";
     FML::CORRELATIONFUNCTIONS::PowerSpectrumBinning<Ndim> pofk(Nmesh / 2);
     FML::CORRELATIONFUNCTIONS::compute_power_spectrum_interlacing<Ndim>(
         Nmesh, part.get_particles_ptr(), part.get_npart(), part.get_npart_total(), pofk, interpolation_method);
 
     //=====================================================
-    // Add back shot-noise subtracted in the routine above
-    // (The signal drowns otherwise)
+    // ...and add back the shot-noise that was subtracted 
+    // in the routine above
     //=====================================================
     for (int i = 0; i < pofk.n; i++) {
         pofk.pofk[i] += 1.0 / double(part.get_npart_total());
     }
 
     //=====================================================
-    // Convert to physical units
+    // Convert P(k) to physical units: inherits the same
+    // units as the boxsize so h/Mpc for k and (Mpc/h)^NDIM for P(k)
     //=====================================================
     if (FML::ThisTask == 0)
-        std::cout << "Pofk / Pofk_input\n";
+        std::cout << "\n#  k (h/Mpc) Pofk / Pofk_input\n";
     pofk.scale(box);
     if (FML::ThisTask == 0) {
         for (int i = 0; i < pofk.n; i++) {
-            double k = pofk.kbin[i];
+            // The mean value in the bin
+            const double k = pofk.kbin[i];
             if (k == 0.0)
                 continue;
             std::cout << k << " " << pofk.pofk[i] / power_spectrum(k) << "\n";
@@ -227,7 +290,8 @@ int main() {
     }
 
     //=====================================================
-    // Free the last of the memory before printing
+    // Free the last of the memory before printing the
+    // memory status (this is not needed)
     //=====================================================
     part.free();
 
