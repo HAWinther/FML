6745f1448d1dc3a9267a16a4dff7d233c7879467
Hans A. Winther <hansw@eduroam-193-157-240-25.wlan.uio.no>
2020-08-27 16:42:30 +0200

    Added N-Body with examples

---
+++ FML/NBody/NBody.h
@@ -0,0 +1,306 @@
+#ifndef NBODY_HEADER
+#define NBODY_HEADER
+
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+#ifdef USE_OMP
+#include <omp.h>
+#endif
+
+#include <FML/FFTWGrid/FFTWGrid.h>
+#include <FML/Global/Global.h>
+
+namespace FML {
+    namespace NBODY {
+
+        // Type alias
+        template <int N>
+        using FFTWGrid = FML::GRID::FFTWGrid<N>;
+
+        template <int N, class T>
+        void DriftParticles(FML::PARTICLE::MPIParticles<T> & part, double delta_time, bool periodic_box = true);
+
+        template <int N, class T>
+        void DriftParticles(T * p, size_t NumPart, double delta_time, bool periodic_box = true);
+
+        template <int N, class T>
+        void KickParticles(std::array<FFTWGrid<N>, N> & force_grid,
+                           FML::PARTICLE::MPIParticles<T> & part,
+                           double delta_time,
+                           std::string interpolation_method);
+
+        template <int N, class T>
+        void KickParticles(std::array<FFTWGrid<N>, N> & force_grid,
+                           T * p,
+                           size_t NumPart,
+                           double delta_time,
+                           std::string interpolation_method);
+
+        template <int N>
+        void compute_force_from_density(const FFTWGrid<N> & density_grid_real,
+                                        std::array<FFTWGrid<N>, N> & force_real,
+                                        double norm_poisson_equation = 1.0);
+
+        //===================================================================================
+        /// @brief Take a N-body step with a naive Kick-Drift method (thus this
+        /// methods should mainly serve as an example for how to do this and not be used for anything serious).
+        /// 1. Particles to grid to get \f$ \delta \f$
+        /// 2. Compute the Newtonian potential via \f$ \nabla^2 \Phi = {\rm norm} \cdot \delta \f$
+        /// 3. Compute the force  \f$ F = \nabla \Phi \f$
+        /// 4. Move the particles using \f$ x \to x + v \Delta t \f$ and \f$ v \to v + F \Delta t \f$
+        /// This method assumes that the velocities are in units of (boxsize / time-step-unit), in other words that \f$
+        /// v\Delta t\f$ gives rise to a shift in [0,1).
+        ///
+        /// @tparam N The dimension of the grid.
+        /// @tparam T The particle class.
+        ///
+        /// @param[in] Nmesh The gridsize to use for computing the density and force.
+        /// @param[out] part The particles
+        /// @param[in] delta_time The time \f$ \Delta t \f$ we move forward.
+        /// @param[in] density_assignment_method The density assignement method (NGP, CIC, TSC, PCS or PQS).
+        /// @param[in] norm_poisson_equation A possible prefactor to the Poisson equation
+        ///
+        //===================================================================================
+        template <int N, class T>
+        void KickDriftNBodyStep(int Nmesh,
+                                FML::PARTICLE::MPIParticles<T> & part,
+                                double delta_time,
+                                std::string density_assignment_method,
+                                double norm_poisson_equation) {
+
+            const bool periodic_box = true;
+
+            // Particles -> density field
+            auto nleftright =
+                FML::INTERPOLATION::get_extra_slices_needed_for_density_assignment(density_assignment_method);
+            FFTWGrid<N> density_grid_real(Nmesh, nleftright.first, nleftright.second);
+            FML::INTERPOLATION::particles_to_grid<N, T>(part.get_particles().data(),
+                                                        part.get_npart(),
+                                                        part.get_npart_total(),
+                                                        density_grid_real,
+                                                        density_assignment_method);
+
+            // Density field -> force
+            std::array<FFTWGrid<N>, N> force_real;
+            compute_force_from_density(density_grid_real, force_real, norm_poisson_equation);
+
+            // Update velocity of particles
+            KickParticles(force_real, part, delta_time, density_assignment_method);
+
+            // Move particles (this does communication)
+            DriftParticles<N, T>(part, delta_time, periodic_box);
+        }
+
+        //===================================================================================
+        /// Take a density grid in real space and returns the force \f$ \nabla \phi \f$  where
+        /// \f$ \nabla^2 \phi = {\rm norm} \cdot \delta \f$
+        ///
+        /// @tparam N The dimension of the grid
+        ///
+        /// @param[in] density_grid_real The density contrast in real space.
+        /// @param[out] force_real The force in real space.
+        /// @param[in] norm_poisson_equation The prefactor (norm) to the Poisson equation.
+        ///
+        //===================================================================================
+        template <int N>
+        void compute_force_from_density(const FFTWGrid<N> & density_grid_real,
+                                        std::array<FFTWGrid<N>, N> & force_real,
+                                        double norm_poisson_equation) {
+
+            // Copy over
+            for (int idim = 0; idim < N; idim++)
+                force_real[idim] = density_grid_real;
+
+            // Density grid to fourier space
+            force_real[0].fftw_r2c();
+
+            auto Local_x_start = density_grid_real.get_local_x_start();
+
+            // Loop over all local fourier grid cells
+            std::array<double, N> kvec;
+            double kmag2;
+            std::complex<double> I(0, 1);
+            for (auto & fourier_index : force_real[0].get_fourier_range()) {
+                force_real[0].get_fourier_wavevector_and_norm2_by_index(fourier_index, kvec, kmag2);
+                auto value = force_real[0].get_fourier_from_index(fourier_index);
+
+                // Multiply by -i/k^2
+                value *= -norm_poisson_equation * I / kmag2;
+                if (Local_x_start == 0 and fourier_index == 0)
+                    value = 0.0;
+
+                // Compute force -ik/k^2 delta(k)
+                for (int idim = 0; idim < N; idim++)
+                    force_real[idim].set_fourier_from_index(fourier_index, value * kvec[idim]);
+            }
+
+            // DC mode
+            if (FML::ThisTask == 0) {
+                for (int idim = 0; idim < N; idim++)
+                    force_real[idim].set_fourier_from_index(0, 0.0);
+            }
+
+            // Fourier transform back to real space
+            for (int idim = 0; idim < N; idim++)
+                force_real[idim].fftw_c2r();
+        }
+
+        //===================================================================================
+        /// This moves the particles according to \f$ x_{\rm new} = x + v \Delta t \f$. Note that we assume the
+        /// velocities are in such units that \f$ v \Delta t\f$ is a dimensionless shift in [0,1).
+        ///
+        /// @tparam N The dimension of the grid
+        /// @tparam T The particle class
+        ///
+        /// @param[out] part MPIParticles containing the particles.
+        /// @param[in] delta_time The size of the timestep.
+        /// @param[in] periodic_box Is the box periodic?
+        ///
+        //===================================================================================
+        template <int N, class T>
+        void DriftParticles(FML::PARTICLE::MPIParticles<T> & part, double delta_time, bool periodic_box) {
+            if (part.get_npart() == 0)
+                return;
+            const int Ndim = part[0].get_ndim();
+            assert(Ndim == N);
+
+            DriftParticles<N, T>(part.get_particles_ptr(), part.get_npart(), delta_time, periodic_box);
+
+            // Particles might have left the current task
+            part.communicate_particles();
+        }
+
+        //===================================================================================
+        /// This moves the particles according to \f$ x_{\rm new} = x + v \Delta t \f$. Note that we assume the
+        /// velocities are in such units that \f$ v \Delta t\f$ is a dimensionless shift in [0,1). NB: after this
+        /// methods is done the particles might have left the current task and must be communicated (this is done
+        /// automatically if you use the MPIParticles version of this method).
+        ///
+        /// @tparam N The dimension of the grid
+        /// @tparam T The particle class
+        ///
+        /// @param[out] p Pointer to the first particle.
+        /// @param[in] NumPart The number of local particles.
+        /// @param[in] delta_time The size of the timestep.
+        /// @param[in] periodic_box Is the box periodic?
+        ///
+        //===================================================================================
+        template <int N, class T>
+        void DriftParticles(T * p, size_t NumPart, double delta_time, bool periodic_box) {
+            if (NumPart == 0)
+                return;
+            const int Ndim = p[0].get_ndim();
+            assert(Ndim == N);
+
+            double max_disp = 0.0;
+#ifdef USE_OMP
+#pragma omp parallel for reduction(max : max_disp)
+#endif
+            for (size_t i = 0; i < NumPart; i++) {
+                auto * pos = p[i].get_pos();
+                auto * vel = p[i].get_vel();
+                for (int idim = 0; idim < N; idim++) {
+                    double disp = vel[idim] * delta_time;
+                    pos[idim] += disp;
+                    max_disp = std::max(max_disp, std::abs(disp));
+
+                    // Periodic wrap
+                    if (periodic_box) {
+                        if (pos[idim] >= 1.0)
+                            pos[idim] -= 1.0;
+                        if (pos[idim] < 0.0)
+                            pos[idim] += 1.0;
+                    }
+                }
+            }
+            FML::MaxOverTasks(&max_disp);
+
+            if (FML::ThisTask == 0)
+                std::cout << "[Drift] Max disp: " << max_disp << "\n";
+        }
+
+        //===================================================================================
+        /// This moves the particle velocities according to \f$ v_{\rm new} = v + F \Delta t \f$. This method assumes
+        /// the force is normalized such that \f$ F \Delta t \f$ has the same units as your v. This method frees up
+        /// memory in the force grids after we have used them. Can be changed with a flag in the source.
+        ///
+        /// @tparam N The dimension of the grid
+        /// @tparam T The particle class
+        ///
+        /// @param[in] force_grid Grid containing the force.
+        /// @param[out] part MPIParticles containing the particles.
+        /// @param[in] delta_time The size of the timestep.
+        /// @param[in] interpolation_method The interpolation method for interpolating the force to the particle
+        /// positions.
+        ///
+        //===================================================================================
+        template <int N, class T>
+        void KickParticles(std::array<FFTWGrid<N>, N> & force_grid,
+                           FML::PARTICLE::MPIParticles<T> & part,
+                           double delta_time,
+                           std::string interpolation_method) {
+
+            KickParticles<N, T>(
+                force_grid, part.get_particles_ptr(), part.get_npart(), delta_time, interpolation_method);
+        }
+
+        //===================================================================================
+        /// This moves the particle velocities according to \f$ v_{\rm new} = v + F \Delta t \f$. This method assumes
+        /// the force is normalized such that \f$ F \Delta t \f$ has the same units as your v. This method frees up
+        /// memory in the force grids after we have used them. Can be changed with a flag in the source.
+        ///
+        /// @tparam N The dimension of the grid
+        /// @tparam T The particle class
+        ///
+        /// @param[out] p Pointer to the first particle.
+        /// @param[in] NumPart The number of local particles.
+        /// @param[in] delta_time The size of the timestep.
+        /// @param[in] interpolation_method The interpolation method for interpolating the force to the particle
+        /// positions.
+        ///
+        //===================================================================================
+        template <int N, class T>
+        void KickParticles(std::array<FFTWGrid<N>, N> & force_grid,
+                           T * p,
+                           size_t NumPart,
+                           double delta_time,
+                           std::string interpolation_method) {
+
+            // Deallocate the force grids (after interpolating to the particles we don't need it here and probably not
+            // elsewhere so lets save some memory)
+            const bool free_force_grids = true;
+
+            // Interpolate force to particle positions
+            std::array<std::vector<double>, N> force;
+            for (int idim = 0; idim < N; idim++) {
+                force_grid[idim].communicate_boundaries();
+                FML::INTERPOLATION::interpolate_grid_to_particle_positions<N, T>(
+                    force_grid[idim], p, NumPart, force[idim], interpolation_method);
+                if (free_force_grids)
+                    force_grid[idim].free();
+            }
+
+            double max_dvel = 0.0;
+#ifdef USE_OMP
+#pragma omp parallel for reduction(max : max_dvel)
+#endif
+            for (size_t i = 0; i < NumPart; i++) {
+                auto * vel = p[i].get_vel();
+                for (int idim = 0; idim < N; idim++) {
+                    double dvel = -force[idim][i] * delta_time;
+                    max_dvel = std::max(max_dvel, std::abs(dvel));
+                    vel[idim] += dvel;
+                }
+            }
+
+            FML::MaxOverTasks(&max_dvel);
+
+            if (FML::ThisTask == 0)
+                std::cout << "[Kick] Max dvel disp: " << max_dvel * delta_time << "\n";
+        }
+
+    } // namespace NBODY
+} // namespace FML
+#endif
---
+++ FML/NBody/example/Main.cpp
@@ -0,0 +1,260 @@
+#include <FML/ComputePowerSpectra/ComputePowerSpectrum.h>
+#include <FML/FFTWGrid/FFTWGrid.h>
+#include <FML/LPT/DisplacementFields.h>
+#include <FML/MPIParticles/MPIParticles.h>
+#include <FML/NBody/NBody.h>
+#include <FML/ParticleTypes/SimpleParticle.h>
+#include <FML/RandomFields/GaussianRandomField.h>
+#include <FML/Spline/Spline.h>
+
+//=====================================================
+//
+// This example shows how to make a very simple and
+// naive N-body solver from a given P(k).
+//
+// We generate particles using 1LPPT and step from the 
+// initial time til z = 0 and compute the power-spectrum 
+// and compare to the input.
+//
+// This is just meant to illustrate how one can do this
+//
+//=====================================================
+
+//=====================================================
+// The dimension we are working in (e.g. Ndim=2 is nice
+// to use for testing as its much faster) and the
+// boxsize in your physical units (say Mpc/h)
+// This only enters in converting the power-spectum 
+// to dimensionless units
+//=====================================================
+
+const int Ndim = 3;
+const double box = 1000.0;
+const double z_ini = 20.0;
+const bool fix_amplitude = true;
+const double OmegaM = 1.0;
+const int Nmesh = 256*4;
+const int Npart_1D = 256*4;
+const double buffer_factor = 2;
+const int nsteps = 40;
+std::string interpolation_method = "CIC";
+std::string filename = "pofk.txt";
+
+//=====================================================
+// Type aliases for easier use below
+//=====================================================
+template <class T>
+using MPIParticles = FML::PARTICLE::MPIParticles<T>;
+template <int N>
+using FFTWGrid = FML::GRID::FFTWGrid<N>;
+using Particle = SimpleParticle<Ndim>;
+
+//=====================================================
+// To generate a gaussian random field we need a RNG generator
+// and a power-spectrum. The P(k) we read in is at z=0
+//=====================================================
+void generate_delta(FFTWGrid<Ndim> & delta, std::string filename_pofk) {
+    FML::RANDOM::RandomGenerator * rng = new FML::RANDOM::RandomGenerator;
+
+    // Read k, P(k) in units of h/Mpc, (Mpc/h)^3
+    std::ifstream fp(filename_pofk.c_str());
+    std::vector<double> logk;
+    std::vector<double> logpofk;
+    for (;;) {
+        double kin, pofkin;
+        fp >> kin;
+        if (fp.eof())
+            break;
+        fp >> pofkin;
+        logk.push_back(std::log(kin));
+        logpofk.push_back(std::log(pofkin));
+    }
+
+    // Make a spline and the P(k) function needed below
+    // We scale this back to the original time using growth-factors
+    // which we just approximate as D = a
+    FML::INTERPOLATION::SPLINE::Spline logpofk_spline(logk, logpofk);
+    std::function<double(double)> Powspec = [&](double kBox) {
+        const double D = 1.0 / (1.0 + z_ini);
+        const double logk = std::log(kBox / box);
+        const double volume = std::pow(box, Ndim);
+        return D * D * std::exp(logpofk_spline(logk)) / volume;
+    };
+
+    // Make a random field in fourier space
+    FML::RANDOM::GAUSSIAN::generate_gaussian_random_field_fourier(delta, rng, Powspec, fix_amplitude);
+}
+
+void generate_particles(FFTWGrid<Ndim> & delta, MPIParticles<Particle> & part) {
+    //=====================================================
+    // Set up a regular grid with particles
+    //=====================================================
+    part.create_particle_grid(Npart_1D, buffer_factor, FML::xmin_domain, FML::xmax_domain);
+    part.info();
+
+    //=====================================================
+    // Generate the 1LPT potential phi_1LPT = delta(k)/k^2
+    //=====================================================
+    FFTWGrid<Ndim> phi_1LPT;
+    FML::COSMOLOGY::LPT::compute_1LPT_potential_fourier(delta, phi_1LPT);
+    delta.free();
+
+    //=====================================================
+    // Generate displacement field Psi = Dphi
+    // 3+3 FFTS
+    //=====================================================
+    std::vector<FFTWGrid<Ndim>> Psi_1LPT_vector;
+    FML::COSMOLOGY::LPT::from_LPT_potential_to_displacement_vector(phi_1LPT, Psi_1LPT_vector);
+    phi_1LPT.free();
+
+    //=====================================================
+    // Interpolate 1LPT displacement field to particle positions
+    // (Note: if Npart_1D = Nmesh then we can assign directly
+    // without interpolation using the disp fields as the
+    // cell is at the particle position and save time though
+    // we don't do this here)
+    //=====================================================
+    std::vector<std::vector<FML::GRID::FloatType>> displacements_1LPT(Ndim);
+    for (int idim = Ndim - 1; idim >= 0; idim--) {
+        if (FML::ThisTask == 0)
+            std::cout << "Assigning particles for idim = " << idim << "\n";
+        FML::INTERPOLATION::interpolate_grid_to_particle_positions(
+            Psi_1LPT_vector[idim], part.get_particles_ptr(), part.get_npart(), displacements_1LPT[idim], "CIC");
+        Psi_1LPT_vector[idim].free();
+    }
+
+    //=====================================================
+    // Set the velocities to be the dimensionless v_code = a^2 dx/dt  / (H0 Box)
+    // For simplicity for the growth rate we just use the approx D1 = a => f1 = 1 and f2 = 2
+    // (easy to compute the exact values by just solving the ODE or using the approx f1 ~ OmegaM(a)^0.55)
+    //=====================================================
+    const double a_ini = 1.0 / (1.0 + z_ini);
+    const double HoverH0 = std::sqrt(OmegaM / (a_ini * a_ini * a_ini) + 1.0 - OmegaM);
+    const double growth_rate1 = std::pow(OmegaM / (a_ini * a_ini * a_ini * HoverH0 * HoverH0), 0.55);
+    const double vfac_1LPT = a_ini * a_ini * HoverH0 * growth_rate1;
+
+    //=====================================================
+    // Add 1LPT displacement to particle position
+    // Note the -3/7 factor. This is because growth factors
+    // are defined to be == 1 at the initial time, however
+    // the physically relevant solution has D2 = -3/7 D1^2
+    // so we need to put this in by hand
+    //=====================================================
+
+    double max_disp_1LPT = 0.0;
+    double max_vel_1LPT = 0.0;
+
+    auto * part_ptr = part.get_particles_ptr();
+#ifdef USE_OMP
+#pragma omp parallel for reduction(max : max_disp_1LPT, max_vel_1LPT)
+#endif
+    for (size_t ind = 0; ind < part.get_npart(); ind++) {
+        auto * pos = part_ptr[ind].get_pos();
+        auto * vel = part_ptr[ind].get_vel();
+        for (int idim = Ndim - 1; idim >= 0; idim--) {
+            const double dpos_1LPT = displacements_1LPT[idim][ind];
+            pos[idim] += dpos_1LPT;
+            vel[idim] = vfac_1LPT * dpos_1LPT;
+            if (std::fabs(dpos_1LPT) > max_disp_1LPT)
+                max_disp_1LPT = std::fabs(dpos_1LPT);
+            if (std::fabs(vfac_1LPT * dpos_1LPT) > max_vel_1LPT)
+                max_vel_1LPT = std::fabs(vfac_1LPT * dpos_1LPT);
+            if (pos[idim] >= 1.0)
+                pos[idim] -= 1.0;
+            if (pos[idim] < 0.0)
+                pos[idim] += 1.0;
+        }
+    }
+
+    //=====================================================
+    // Output the maximum displacment and velocity
+    //=====================================================
+    FML::MaxOverTasks(&max_disp_1LPT);
+    FML::MaxOverTasks(&max_vel_1LPT);
+    if (FML::ThisTask == 0)
+        std::cout << "Maximum displacements: " << max_disp_1LPT * box << " Mpc/h\n";
+    if (FML::ThisTask == 0)
+        std::cout << "Maximum velocity: " << max_vel_1LPT * 100.0 * box << " km/s comoving\n";
+
+    //=====================================================
+    // Communicate particles (they might have left the
+    // current task)
+    //=====================================================
+    part.communicate_particles();
+}
+
+int main() {
+
+    //=====================================================
+    // Generate density field
+    //=====================================================
+    auto nextra = FML::INTERPOLATION::get_extra_slices_needed_for_density_assignment(interpolation_method);
+    FFTWGrid<Ndim> delta(Nmesh, nextra.first, nextra.second);
+    generate_delta(delta, filename);
+
+    //===============================Y======================
+    // Make a regular (Lagrangian) particle grid with
+    // Npart_1D^NDIM particles in total
+    //=====================================================
+    MPIParticles<Particle> part;
+    generate_particles(delta, part);
+
+    // Start the main time-stepping loop
+    double aini = 1.0 / (1.0 + z_ini);
+    double aend = 1.0;
+    const double delta_aexp = (aend - aini) / double(nsteps);
+    auto HoverH0 = [&](double a) { return std::sqrt(OmegaM / (a * a * a) + 1.0 - OmegaM); };
+
+    for (int i = 0; i < nsteps; i++) {
+
+        // Scale factor for velocity and positions
+        double apos_old = aini + delta_aexp * i;
+        double apos_new = apos_old + delta_aexp;
+        double avel_old = i == 0 ? aini : apos_old - 0.5 * delta_aexp;
+        double avel_new = i == 0 ? aini + 0.5 * delta_aexp : apos_old + 0.5 * delta_aexp;
+
+        // Timestep: first step we displace pos and vel by 1/2 timestep to do leapfrog
+        double delta_time_pos = (apos_new - apos_old) / (HoverH0(avel_new) * std::pow(avel_new, 3));
+        double delta_time_vel = (avel_new - avel_old) / (HoverH0(apos_old) * std::pow(apos_old, 3));
+        const double norm_poisson_equation = 1.5 * OmegaM * apos_old;
+
+        // Particles -> density field
+        auto nleftright = FML::INTERPOLATION::get_extra_slices_needed_for_density_assignment(interpolation_method);
+        FFTWGrid<Ndim> density_grid_real(Nmesh, nleftright.first, nleftright.second);
+        FML::INTERPOLATION::particles_to_grid<Ndim, Particle>(part.get_particles().data(),
+                                                              part.get_npart(),
+                                                              part.get_npart_total(),
+                                                              density_grid_real,
+                                                              interpolation_method);
+
+        // Density field -> force
+        std::array<FFTWGrid<Ndim>, Ndim> force_real;
+        FML::NBODY::compute_force_from_density(density_grid_real, force_real, norm_poisson_equation);
+
+        // Update velocity of particles (frees force)
+        FML::NBODY::KickParticles(force_real, part, delta_time_vel, interpolation_method);
+
+        // Move particles (this does communication)
+        FML::NBODY::DriftParticles<Ndim, Particle>(part, delta_time_pos);
+
+        if (FML::ThisTask == 0)
+            std::cout << i + 1 << " / " << nsteps << " done! a: " << apos_new << "\n";
+    }
+
+    // Velocities are here out of sync with positions (we don't bother fixing this)
+
+    // Compute power-spectrum
+    FML::CORRELATIONFUNCTIONS::PowerSpectrumBinning<Ndim> pofk(Nmesh / 2);
+    FML::CORRELATIONFUNCTIONS::compute_power_spectrum_interlacing<Ndim>(
+        Nmesh, part.get_particles_ptr(), part.get_npart(), part.get_npart_total(), pofk, "PQS");
+    //for (int i = 0; i < pofk.n; i++)
+    //    pofk.pofk[i] += 1.0 / double(part.get_npart_total());
+    pofk.scale(box);
+    if (FML::ThisTask == 0) {
+        for (int i = 0; i < pofk.n; i++) {
+            const double k = pofk.kbin[i];
+            std::cout << k << " " << pofk.pofk[i] << "\n";
+        }
+    }
+}
+
