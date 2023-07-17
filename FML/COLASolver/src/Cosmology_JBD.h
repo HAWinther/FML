1255e7a70a2dcbc6d8dbd5b2c270f05e0fc7b651
Herman Sletmoen <hermansletmoen@gmail.com>
2023-07-17 19:24:09 +0200

    JBD: improve scalar field integration, solve quadratic equation for dlogphi_dloga

--- FML/COLASolver/src/Cosmology_JBD.h
+++ FML/COLASolver/src/Cosmology_JBD.h
@@ -32,16 +32,14 @@ class CosmologyJBD final : public Cosmology {
         double phi_today_target = 1.0 / GeffG_today; // TODO: w-factors
         double phi_ini_lo = 0.0;
         double phi_ini_hi = phi_today_target;
-        double OmegaPhi = 0.0; // bootstrap the first guess for OmegaLambda in the loop
+        OmegaLambda = 1.0 - OmegaR - Omegab - OmegaCDM - OmegaK; // initial guess for OmegaLambda (neglecting neutrinos and scalar field)
 
         std::cout << "JBD::init Searching for phi_ini and OmegaLambda that gives "
                   << "phi_today = " << phi_today_target << " and "
                   << "E_today = (H/H0)_today = 1.0:" << "\n";
 
         for (int iter = 1; iter < 100; iter++) {
-            // Refine guesses for phi_ini (from bisection limits) and OmegaLambda (from closure condition E0 == 1)
-            phi_ini = (phi_ini_lo + phi_ini_hi) / 2.0;
-            OmegaLambda = 1.0 - OmegaM - OmegaRtot - OmegaK - OmegaPhi; // equivalent to E0 == 1
+            phi_ini = (phi_ini_lo + phi_ini_hi) / 2.0; // try new phi_ini between bisection limits
 
             // Solve cosmology for current (phi_ini, OmegaLambda) and record phi and its derivative today
             init_current();
@@ -59,13 +57,14 @@ class CosmologyJBD final : public Cosmology {
                 return; // hit, so stop; the cosmology is initialized and ready-to-use
             }
 
-            // Improve next guesses for phi_ini and OmegaLambda
+            // Refine guesses for phi_ini (from bisection limits) and OmegaLambda (from closure condition E0 == 1)
             if (phi_of_a(1.0) < phi_today_target) {
                 phi_ini_lo = phi_ini; // underhit, so increase next guess
             } else {
                 phi_ini_hi = phi_ini; //  overhit, so decrease next guess
             }
-            OmegaPhi = -dlogphi_dloga_of_a(1.0) + wBD/6 * dlogphi_dloga_of_a(1.0) * dlogphi_dloga_of_a(1.0); // defined from E0 == 1, so sum_i Omega_i == 1
+            double OmegaPhi = -dlogphi_dloga_of_a(1.0) + wBD/6 * dlogphi_dloga_of_a(1.0) * dlogphi_dloga_of_a(1.0); // defined from E0 == 1, so sum_i Omega_i == 1
+            OmegaLambda = 1.0 - OmegaR - this->get_rhoNu_exact(1.0) - Omegab - OmegaCDM - OmegaK - OmegaPhi; // equivalent to E0 == 1 // TODO: correct/improve neutrino treatment?
         }
 
         throw std::runtime_error("JBD::init Search for phi_ini and OmegaLambda did not converge");
@@ -77,60 +76,64 @@ class CosmologyJBD final : public Cosmology {
     void init_current() {
         Cosmology::init();
 
-        // Linearly spaced scale factor logarithms to integrate over
+        // Linearly spaced scale factor logarithms to integrate over // TODO: linspace?
         const double dloga = (loga_max - loga_min) / double(nloga - 1);
         DVector loga_arr(nloga);
         for (int i = 0; i < nloga; i++) {
             loga_arr[i] = loga_min + i * dloga;
         }
 
-        // E = H/H0 as function of loga, y = logphi and z = E a^3 e^y dy/dx. phi is here phi/phi0
-        auto JBD_E_of_z = [&](double loga, double logphi, double z) {
-            const double a = std::exp(loga);
-            return std::sqrt(exp(-logphi) *
-                                 (OmegaK / (a * a) + OmegaR / (a * a * a * a) + this->get_rhoNu_exact(a) + // TODO: phi * Omegak
-                                  (Omegab + OmegaCDM) / (a * a * a) + OmegaLambda) + // TODO: OmegaM?
-                             z * z / 12.0 * (2.0 * wBD + 3.0) * exp(-2 * logphi - 6 * loga)) -
-                   z / 2.0 * exp(-logphi - 3 * loga);
+        // Convenience functions for calculating E = sqrt(E2_frac_top / E2_frac_bot)
+        auto E2_frac_top = [&](double loga, double logphi) {
+            double a = std::exp(loga);
+            double phi = std::exp(logphi);
+            return OmegaR / (a*a*a*a) + this->get_rhoNu_exact(a) + (Omegab+OmegaCDM) / (a*a*a) + phi * OmegaK / (a*a) + OmegaLambda; // TODO: correct/improve neutrino treatment?
+        };
+        auto E2_frac_bot = [&](double logphi, double dlogphi_dloga) {
+            double phi = std::exp(logphi);
+            return phi * (1.0 + dlogphi_dloga - wBD/6 * dlogphi_dloga * dlogphi_dloga);
+        };
+        auto E_func = [&](double loga, double logphi, double dlogphi_dloga) {
+            return std::sqrt(E2_frac_top(loga, logphi) / E2_frac_bot(logphi, dlogphi_dloga));
         };
 
-        // H as function of loga, logphi and dlogphidloga. phi is here phi/phi0
-        auto JBD_E_of_dy = [&](double loga, double logphi, double dlogphidloga) {
-            const double a = std::exp(loga);
-            return std::sqrt(std::exp(-logphi) *
-                             (OmegaR / (a * a * a * a) + this->get_rhoNu_exact(a) + // TODO: phi * Omegak
-                              (Omegab + OmegaCDM) / (a * a * a) + OmegaLambda) / // TODO: OmegaM?
-                             (1.0 + dlogphidloga - wBD / 6 * dlogphidloga * dlogphidloga));
+        // ODE system for scalar field phi (here "phi" means phi/phi0 TODO: does it, really?):
+        // y0' = d(logphi)                  / dloga
+        // y1' = d(a^3*E*phi*dlogphi/dloga) / dloga = 3/(3+2*wBD) * (OmegaM + 4*OmegaLambda*a^3) / E
+        FML::SOLVERS::ODESOLVER::ODEFunction deriv = [&](double loga, const double * y, double * dy_dloga) {
+            double a = std::exp(loga);
+
+            // logphi is y0
+            double logphi = y[0];
+            double phi = std::exp(logphi);
+
+            // dlogphi_dloga is solution of y1 == a^3 * E(dlogphi_dloga) * phi * dlogphi_dloga
+            // Squaring it and expanding E2(dlogphi_dloga) = E2_frac_top / E2_frac_bot(dlogphi_dloga),
+            // it becomes a quadratic equation that can be solved exactly
+            double A = phi * a*a*a*a*a*a * E2_frac_top(loga, logphi) + wBD/6 * y[1]*y[1];
+            double B = -y[1]*y[1];
+            double C = -y[1]*y[1];
+            double dlogphi_dloga = (-B + std::sqrt(B*B - 4*A*C)) / (2*A); // + to take positive solution
+
+            dy_dloga[0] = dlogphi_dloga;
+            dy_dloga[1] = 3.0 / (3.0+2.0*wBD) * (OmegaM + 4.0 * OmegaLambda * a*a*a) / E_func(loga, logphi, dlogphi_dloga);
+            return GSL_SUCCESS;
         };
 
-        // Solve the ODE for a given initial value
-        auto solve_ode = [&](double _logphi_ini, double _dlogphidloga_ini) {
-            // The neutrino treatment here is not 100%, but good enough
-            // Should correct this at some point though
-            FML::SOLVERS::ODESOLVER::ODEFunction deriv = [&](double loga, const double * y, double * dydx) {
-                double E = JBD_E_of_z(loga, y[0], y[1]);
-                dydx[0] = y[1] * std::exp(-y[0] - 3 * loga) / E;
-                dydx[1] = 3.0 * std::exp(3.0 * loga) / (2 * wBD + 3) / E *
-                          ((Omegab + OmegaCDM + OmegaMNu) * std::exp(-3 * loga) + 4.0 * OmegaLambda); // TODO: OmegaM?
-                return GSL_SUCCESS;
-            };
-            FML::SOLVERS::ODESOLVER::ODESolver ode;
-            const double loga_ini = loga_min;
-            const double z_ini = JBD_E_of_dy(loga_ini, _logphi_ini, _dlogphidloga_ini) *
-                                 std::exp(3.0 * loga_ini + _logphi_ini) * _dlogphidloga_ini;
-            DVector ini{_logphi_ini, z_ini};
-            ode.solve(deriv, loga_arr, ini);
-            return ode.get_data_by_component(0);
-        };
+        // Integrate scalar field phi
+        FML::SOLVERS::ODESOLVER::ODESolver ode;
+        DVector yini{std::log(phi_ini), 0.0}; // assume dlogphi_dloga = 0 at early times // TODO: explain why
+        ode.solve(deriv, loga_arr, yini);
+        auto logphi_arr = ode.get_data_by_component(0);
 
-        // Solve the ODE for this initial value and spline logphi(loga) and logE(loga)
-        auto logphi_arr = solve_ode(std::log(phi_ini), 0.0);
+        // Spline logphi(loga)
         logphi_of_loga_spline.create(loga_arr, logphi_arr, "JBD logphi(loga)");
 
+        // Spline logE(loga)
         auto logE_of_loga_arr = loga_arr; // copy to create vector of same size (preserving loga)
         for (auto & logE : logE_of_loga_arr) {
             double loga = logE; // because we copied the vector above
-            logE = std::log(JBD_E_of_dy(loga, logphi_of_loga_spline(loga), logphi_of_loga_spline.deriv_x(loga)));
+            logE = std::log(E_func(loga, logphi_of_loga_spline(loga), logphi_of_loga_spline.deriv_x(loga)));
         }
         logE_of_loga_spline.create(loga_arr, logE_of_loga_arr, "JBD logE(loga)");
     }
@@ -177,4 +180,3 @@ class CosmologyJBD final : public Cosmology {
     const int nloga = 1000;
 };
 #endif
-
